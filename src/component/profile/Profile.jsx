e8a3447cdd9d3d74f3d9072c06c49ff384ef710d
DulangaMendis <dulangamendis82@gmail.com>
2024-11-06 13:04:21 +0530

    create profile routers

--- src/component/profile/Profile.jsx
+++ src/component/profile/Profile.jsx
@@ -1,21 +1,35 @@
 import React, { useState } from 'react'
 import { ProfileNavigation } from './ProfileNavigation'
+import { Route, Routes } from 'react-router-dom';
+import { UserProfile } from './UserProfile';
+import { Orders } from './Orders';
+import { Address } from './Address';
+import { Favourites } from './Favourites';
+import { Events } from './Events';
 
 export const Profile = () => {
-    const [openSideBar, setOpenSideBar]=useState(false);
-  return (
-    <div className='lg:flex justify-between'>
-        <div className='sticky h-[80vh] lg:w-[20%]'>
+    const [openSideBar, setOpenSideBar] = useState(false);
+    return (
+        <div className='lg:flex justify-between'>
+            <div className='sticky h-[80vh] lg:w-[20%]'>
 
-            <ProfileNavigation open={openSideBar}></ProfileNavigation>
+                <ProfileNavigation open={openSideBar}></ProfileNavigation>
+
+            </div>
+
+            <div className='lg:w-[80%]'>
+                <Routes>
+                    <Route path='/' element={<UserProfile />}></Route>
+                    <Route path='/orders' element={<Orders />}></Route>
+                    <Route path='/address' element={<Address />}></Route>
+                    <Route path='/favourites' element={<Favourites />}></Route>
+                    <Route path='/events' element={<Events />}></Route>
+                    
+                </Routes>
+
+
+            </div>
 
         </div>
-
-        <div className='lg:w-[80%]'>
-
-
-        </div>
-
-    </div>
-  )
+    )
 }
--- src/component/profile/ProfileNavigation.jsx
+++ src/component/profile/ProfileNavigation.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import ShoppingBagIcon from '@mui/icons-material/ShoppingBag';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import HomeIcon from '@mui/icons-material/Home';
@@ -7,6 +7,7 @@ import NotificationsIcon from '@mui/icons-material/Notifications';
 import EventIcon from '@mui/icons-material/Event';
 import LogoutIcon from '@mui/icons-material/Logout';
 import { Divider, Drawer, useMediaQuery } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 
 const menu = [
     { title: "Orders", icon: <ShoppingBagIcon /> },
@@ -16,38 +17,40 @@ const menu = [
     { title: "Notifications", icon: <NotificationsIcon /> },
     { title: "Events", icon: <EventIcon /> },
     { title: "Logout", icon: <LogoutIcon /> }
-
-]
+];
 
 export const ProfileNavigation = ({ open, handleClose }) => {
-    const isSmallScreen = useMediaQuery("(max-width:900px)")
+    const isSmallScreen = useMediaQuery("(max-width:900px)");
+    const navigate = useNavigate();  // Call useNavigate as a function here
 
+    const handleNavigate = (item) => {
+        navigate(`/my-profile/${item.title.toLowerCase()}`);
+    };
 
     return (
         <div>
-            <Drawer 
-            variant={isSmallScreen ? "temporary" : "permanent"}
-             onClose={handleClose} 
-             open={ isSmallScreen ? open : "true"} 
-             anchor='left' 
-             sx={{ zIndex: 1 ,position:"sticky" }} >
+            <Drawer
+                variant={isSmallScreen ? "temporary" : "permanent"}
+                onClose={handleClose}
+                open={isSmallScreen ? open : true}
+                anchor='left'
+                sx={{ zIndex: 1, position: "sticky" }}
+            >
                 <div className='w-[50vw] lg:w-[20vw] h-[100vh] flex flex-col justify-center text-xl gap-8 pt-16'>
-                    {menu.map((item, i) => <>
-
-                        <div className='px-5 flex items-center space-x-5 cursor-pointer'>
-                            {item.icon}
-                            <span>{item.title}</span>
-
-                        </div>
-                        {i !== menu.length - 1 && <Divider />}
-
-
-                    </>)}
-
+                    {menu.map((item, i) => (
+                        <React.Fragment key={i}>
+                            <div
+                                onClick={() => handleNavigate(item)}
+                                className='px-5 flex items-center space-x-5 cursor-pointer'
+                            >
+                                {item.icon}
+                                <span>{item.title}</span>
+                            </div>
+                            {i !== menu.length - 1 && <Divider />}
+                        </React.Fragment>
+                    ))}
                 </div>
-
-
             </Drawer>
         </div>
-    )
-}
+    );
+};
