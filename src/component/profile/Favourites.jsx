5ab54841e6a3d6ae4e47d37882cdefbc22afae8e
DulangaMendis <dulangamendis82@gmail.com>
2024-11-06 14:10:14 +0530

    design favourit section

--- src/component/profile/Favourites.jsx
+++ src/component/profile/Favourites.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import { RestaurantCart } from '../Navbar/Restaurant/RestaurantCart'
 
 export const Favourites = () => {
-  return (
-    <div>Favourites</div>
-  )
+    return (
+        <div>
+            <h1 className='py-5 text-xl font-semibold text-center'>My Favourites</h1>
+
+            <div className='flex flex-wrap justify-center gap-5'>
+
+                {[1, 1, 1].map((item) => <RestaurantCart />)}
+
+            </div>
+        </div>
+    )
 }
--- src/component/profile/OrderCard.jsx
+++ src/component/profile/OrderCard.jsx
@@ -2,24 +2,24 @@ import { Button, Card } from '@mui/material'
 import React from 'react'
 
 export const OrderCard = () => {
-  return (
-    <Card className='flex justify-between items-center p-5'>
-        <div className='flex items-center space-x-5'>
-            <img
-            className='h-16 w-16' 
-            src='https://media.istockphoto.com/id/1410130688/photo/mutton-biryani-served-in-a-golden-dish-isolated-on-dark-background-side-view-indian-food.jpg?b=1&s=612x612&w=0&k=20&c=DiaW0yTvt1-ydUk_f_i7YyJ8KoS8ODeHeGlJawwS0o0=' 
-            alt=''/>
+    return (
+        <Card className='flex justify-between items-center p-5'>
+            <div className='flex items-center space-x-5'>
+                <img
+                    className='h-16 w-16'
+                    src='https://media.istockphoto.com/id/1410130688/photo/mutton-biryani-served-in-a-golden-dish-isolated-on-dark-background-side-view-indian-food.jpg?b=1&s=612x612&w=0&k=20&c=DiaW0yTvt1-ydUk_f_i7YyJ8KoS8ODeHeGlJawwS0o0='
+                    alt='' />
 
+                <div>
+                    <p>Biriyani</p>
+                    <p>LKR 1800</p>
+                </div>
+
+            </div>
             <div>
-                <p>Biriyani</p>
-                <p>LKR 1800</p>
+                <Button className='cursor-not-allowed'>completed</Button>
             </div>
 
-        </div>
-        <div>
-            <Button className='cursor-not-allowed'>completed</Button>
-        </div>
-
-    </Card>
-  )
+        </Card>
+    )
 }
